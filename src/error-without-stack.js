b9d82dabd907a4a8a53196ddec8c674fe1cbae41
Wes Cossick <wes@sparksuite.com>
2019-03-08 10:10:38 -0600

    Fix code style issues

--- src/error-without-stack.js
+++ src/error-without-stack.js
@@ -1,7 +1,7 @@
 // Custom error class that does not print a stack
 module.exports = class ErrorWithoutStack extends Error {
-    constructor(...args) {
-        super(...args);
+	constructor(...args) {
+		super(...args);
 		this.stack = this.message;
-    }
+	}
 };

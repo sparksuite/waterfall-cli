337651e26dcbb34f5967c4021dad0b62a48d2ec4
Wes Cossick <wes@sparksuite.com>
2019-10-03 15:11:59 -0500

    Don't run the ES6 import program tests if Node.js is older than v12

--- test/es6-imports.test.js
+++ test/es6-imports.test.js
@@ -2,9 +2,19 @@
 
 // Dependencies
 const runProgram = require('./run-program');
+const semver = require('semver');
 
 // Initialize
 const entryFile = `${__dirname}/programs/es6-imports/cli/entry.js`;
+
+// Don't run the normal tests if older than Node.js version 12
+if (semver.lt(semver.clean(process.version), '12.0.0')) {
+	test('Tests disabled on Node.js versions older than v12', () => {
+		expect(true).toBe(true);
+	});
+	
+	process.exit();
+}
 
 // Tests
 describe('ES6 imports', () => {

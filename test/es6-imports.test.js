317000cffed081d366bfeda7ac5c038ed70f1a13
Wes Cossick <wes@sparksuite.com>
2019-10-03 15:13:16 -0500

    Unwrap the top-level group from each test file

--- test/es6-imports.test.js
+++ test/es6-imports.test.js
@@ -17,14 +17,12 @@ if (semver.lt(semver.clean(process.version), '12.0.0')) {
 }
 
 // Tests
-describe('ES6 imports', () => {
-	describe('Built-in abilities', () => {
-		test('Displays version', () => {
-			return runProgram(entryFile, '--version', '--experimental-modules').then(
-				({ stdout }) => {
-					expect(stdout.includes('es6-imports: 1.2.3')).toBe(true);
-				}
-			);
-		});
+describe('Built-in abilities', () => {
+	test('Displays version', () => {
+		return runProgram(entryFile, '--version', '--experimental-modules').then(
+			({ stdout }) => {
+				expect(stdout.includes('es6-imports: 1.2.3')).toBe(true);
+			}
+		);
 	});
 });
--- test/pizza-ordering.test.js
+++ test/pizza-ordering.test.js
@@ -7,36 +7,34 @@ const runProgram = require('./run-program');
 const entryFile = `${__dirname}/programs/pizza-ordering/cli/entry.js`;
 
 // Tests
-describe('Pizza ordering', () => {
-	describe('Built-in abilities', () => {
-		test('Displays version', () => {
-			return runProgram(entryFile, '--version').then(({ stdout }) => {
-				expect(stdout.includes('pizza-ordering: 1.2.3')).toBe(true);
-			});
+describe('Built-in abilities', () => {
+	test('Displays version', () => {
+		return runProgram(entryFile, '--version').then(({ stdout }) => {
+			expect(stdout.includes('pizza-ordering: 1.2.3')).toBe(true);
 		});
+	});
 
-		test('Displays version for a command', () => {
-			return runProgram(entryFile, 'list --version').then(({ stdout }) => {
-				expect(stdout.includes('pizza-ordering: 1.2.3')).toBe(true);
-			});
+	test('Displays version for a command', () => {
+		return runProgram(entryFile, 'list --version').then(({ stdout }) => {
+			expect(stdout.includes('pizza-ordering: 1.2.3')).toBe(true);
 		});
+	});
 
-		test('Displays help screen', () => {
-			return runProgram(entryFile, '--help').then(({ stdout }) => {
-				expect(stdout.includes('Usage: node entry.js [commands]')).toBe(true);
-			});
+	test('Displays help screen', () => {
+		return runProgram(entryFile, '--help').then(({ stdout }) => {
+			expect(stdout.includes('Usage: node entry.js [commands]')).toBe(true);
 		});
+	});
 
-		test('Displays help screen for a command', () => {
-			return runProgram(entryFile, 'list --help').then(({ stdout }) => {
-				expect(stdout.includes('Usage: node entry.js list [flags]')).toBe(true);
-			});
+	test('Displays help screen for a command', () => {
+		return runProgram(entryFile, 'list --help').then(({ stdout }) => {
+			expect(stdout.includes('Usage: node entry.js list [flags]')).toBe(true);
 		});
+	});
 
-		test('Displays help screen when no arguments', () => {
-			return runProgram(entryFile, '').then(({ stdout }) => {
-				expect(stdout.includes('Usage: node entry.js [commands]')).toBe(true);
-			});
+	test('Displays help screen when no arguments', () => {
+		return runProgram(entryFile, '').then(({ stdout }) => {
+			expect(stdout.includes('Usage: node entry.js [commands]')).toBe(true);
 		});
 	});
 });

80f43318a9c4947ce305578a805d8a28e2740ecb
Peter <peter@hoa-express.com>
2019-10-07 18:30:58 -0500

    Reduce repetition of console.error capture

--- test/print-pretty-error.test.js
+++ test/print-pretty-error.test.js
@@ -3,62 +3,47 @@
 // Dependencies
 const printPrettyError = require('../dist/print-pretty-error.js');
 const chalk = require('chalk');
-
 const mockConsole = require('jest-mock-console').default;
+
+// Prepare to capture console error output
+let restoreConsole;
+let consoleResult;
+
+beforeEach(() => {
+	// Reset captured output
+	consoleResult = '';
+
+	// Configure capture of console.error output
+	restoreConsole = mockConsole();
+	mockConsole({
+		error: string => {
+			consoleResult += string;
+		},
+	});
+});
+
+afterEach(() => {
+	// Revert console back to normal
+	restoreConsole();
+});
 
 // Tests
 describe('#printPrettyError()', () => {
 	test('Has the ERROR indicator', () => {
-		const restoreConsole = mockConsole();
-
-		let result = '';
-
-		mockConsole({
-			error: string => {
-				result += string;
-			},
-		});
-
 		printPrettyError('Error message content');
 
-		expect(result).toContain(chalk.inverse.red.bold(' ERROR '));
-
-		restoreConsole();
+		expect(consoleResult).toContain(chalk.inverse.red.bold(' ERROR '));
 	});
 
 	test('Has the indent of the message', () => {
-		const restoreConsole = mockConsole();
-
-		let result = '';
-
-		mockConsole({
-			error: string => {
-				result += string;
-			},
-		});
-
 		printPrettyError('Error message content');
 
-		expect(result).toContain('> ');
-
-		restoreConsole();
+		expect(consoleResult).toContain('> ');
 	});
 
 	test('Has the message text', () => {
-		const restoreConsole = mockConsole();
-
-		let result = '';
-
-		mockConsole({
-			error: string => {
-				result += string;
-			},
-		});
-
 		printPrettyError('Error message content');
 
-		expect(result).toContain('Error message content');
-
-		restoreConsole();
+		expect(consoleResult).toContain('Error message content');
 	});
 });

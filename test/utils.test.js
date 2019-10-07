007f5d0a7036158d12c5be8890b3a625a3230e16
Peter <peter@hoa-express.com>
2019-10-07 18:05:33 -0500

    Move processArguments related tests to their own test

--- test/utils.test.js
+++ test/utils.test.js
@@ -3,60 +3,8 @@
 // Dependencies
 const defaultSettings = require('../dist/default-settings.js');
 const utils = require('../dist/utils.js');
-const processArguments = require('../dist/process-arguments.js');
 
 // Tests
-describe('#processArguments()', () => {
-	test('Handles normal arguments', () => {
-		expect(
-			processArguments([
-				'/path/to/node',
-				'/path/to/entry.js',
-				'command',
-				'--option1=value',
-				'--option2',
-				'value',
-				'--flag',
-				'-f',
-				'data1',
-				'data2',
-			])
-		).toStrictEqual([
-			'command',
-			'--option1',
-			'value',
-			'--option2',
-			'value',
-			'--flag',
-			'-f',
-			'data1',
-			'data2',
-		]);
-	});
-
-	test('Handles data with special characters', () => {
-		expect(
-			processArguments([
-				'/path/to/node',
-				'/path/to/entry.js',
-				'command',
-				'~!@#$%^&*()-=_+',
-			])
-		).toStrictEqual(['command', '~!@#$%^&*()-=_+']);
-	});
-
-	test('Handles dangling equals sign', () => {
-		expect(
-			processArguments([
-				'/path/to/node',
-				'/path/to/entry.js',
-				'command',
-				'--option=',
-			])
-		).toStrictEqual(['command', '--option']);
-	});
-});
-
 describe('#retrieveAppInformation()', () => {
 	test('Retrieves app info', () => {
 		const settings = {

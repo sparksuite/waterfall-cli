a0abc818476dba354d80edda84d848a2b27ff95e
Peter <peter@hoa-express.com>
2019-10-08 13:05:33 -0500

    Allow es6 module to use commonjs module

--- test/programs/es6-imports/cli/entry.js
+++ test/programs/es6-imports/cli/entry.js
@@ -1,6 +1,10 @@
-// Import and initialize Waterfall CLI
-import waterfallCLI from '../../../../dist/index.js';
+// Import and initialize Waterfall CLI for an es6 module importing a commonjs module
+// -> see: https://nodejs.org/api/modules.html#modules_module_createrequire_filename
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
 
-waterfallCLI.default({
+const waterfallCLI = require('../../../../dist/index.js').default;
+
+waterfallCLI({
 	verbose: true,
 });

0a2dd2ed4d898ae12a8ab993b93a16ec5f19bdee
Peter <peter@hoa-express.com>
2019-10-10 15:12:42 -0500

    Test program scenarios that demonstrate good and bad `onStart` property usage

---
+++ test/programs/onstart/bad/cli/list/list.js
@@ -0,0 +1,8 @@
+// Require Waterfall CLI
+const waterfall = require('../../../../../../dist/index');
+
+// Parse command input
+const input = waterfall.command();
+
+// Output result
+console.log(input);

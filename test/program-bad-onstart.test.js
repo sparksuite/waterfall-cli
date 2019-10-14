7db23c8ba28000b9e534861535f128fda97f7281
Wes Cossick <wes@sparksuite.com>
2019-10-14 09:46:41 -0500

    Format code

--- test/program-bad-onstart.test.js
+++ test/program-bad-onstart.test.js
@@ -9,11 +9,15 @@ const entryFile = `${__dirname}/programs/bad-onstart/cli/entry.js`;
 // Tests
 describe('Settings', () => {
 	test('Crashes with invalid `onStart` function', () => {
-		return runProgram(entryFile, 'example').then(() => {
-			throw new Error('This program should not resolve!')
-		}).catch(({ error, stderr }) => {
-			expect(error.code).toBe(1);
-			expect(stderr.includes('settings.onStart is not a function')).toBe(true);
-		});
+		return runProgram(entryFile, 'example')
+			.then(() => {
+				throw new Error('This program should not resolve!');
+			})
+			.catch(({ error, stderr }) => {
+				expect(error.code).toBe(1);
+				expect(stderr.includes('settings.onStart is not a function')).toBe(
+					true
+				);
+			});
 	});
 });

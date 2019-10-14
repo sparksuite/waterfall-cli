631a64a56f2970cdff84c0d894dd2c6e33085aa0
Wes Cossick <wes@sparksuite.com>
2019-10-14 09:45:53 -0500

    Remove unused variables

--- test/program-bad-onstart.test.js
+++ test/program-bad-onstart.test.js
@@ -9,9 +9,9 @@ const entryFile = `${__dirname}/programs/bad-onstart/cli/entry.js`;
 // Tests
 describe('Settings', () => {
 	test('Crashes with invalid `onStart` function', () => {
-		return runProgram(entryFile, 'example').then(({ stdout }) => {
+		return runProgram(entryFile, 'example').then(() => {
 			throw new Error('This program should not resolve!')
-		}).catch(({ error, stdout, stderr }) => {
+		}).catch(({ error, stderr }) => {
 			expect(error.code).toBe(1);
 			expect(stderr.includes('settings.onStart is not a function')).toBe(true);
 		});

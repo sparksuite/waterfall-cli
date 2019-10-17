0c29dbe4b52269b1f779bc35c4aa91df2814e098
Wes Cossick <wes@sparksuite.com>
2019-10-17 13:08:17 -0500

    Format code

--- test/program-typescript.test.js
+++ test/program-typescript.test.js
@@ -9,7 +9,7 @@ const entryFile = `${__dirname}/programs/typescript/cli/entry.js`;
 // Tests
 describe('Built-in abilities', () => {
 	test('Displays version', () => {
-		return runProgram('node '+entryFile+' --version').then(({ stdout }) => {
+		return runProgram(`node ${entryFile} --version`).then(({ stdout }) => {
 			expect(stdout).toContain('typescript: 1.2.3');
 		});
 	});
@@ -17,7 +17,7 @@ describe('Built-in abilities', () => {
 
 describe('Commands', () => {
 	test('Runs a simple command successfully', () => {
-		return runProgram('node '+entryFile+' example').then(({ stdout }) => {
+		return runProgram(`node ${entryFile} example`).then(({ stdout }) => {
 			expect(stdout).toContain('Ran example command');
 		});
 	});

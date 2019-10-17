4cc3334f2665f09f6b050977b6c0404e7f3bcc98
Wes Cossick <wes@sparksuite.com>
2019-10-17 13:26:58 -0500

    Introduce test that reveals global bin bug

---
+++ test/program-global-bin.test.js
@@ -0,0 +1,47 @@
+/* eslint-env jest */
+
+// Dependencies
+const runProgram = require('./run-program');
+const path = require('path');
+const { exec } = require('child_process');
+
+// Link this program
+beforeAll(() => {
+	return new Promise((resolve, reject) => {
+		const programPath = path.join(__dirname, 'programs', 'global-bin');
+		
+		exec(`cd ${programPath} && npm link`, error => {
+			if (error) {
+				reject();
+				return;
+			}
+
+			resolve();
+		});
+	});
+});
+
+// Tests
+describe('Built-in abilities', () => {
+	test('Displays version', () => {
+		return runProgram('global-bin --version').then(({ stdout }) => {
+			expect(stdout).toContain('global-bin: 1.2.3');
+		});
+	});
+});
+
+// Uninstall this program
+afterAll(() => {
+	return new Promise((resolve, reject) => {
+		const programPath = path.join(__dirname, 'programs', 'global-bin');
+
+		exec(`npm uninstall --global global-bin`, error => {
+			if (error) {
+				reject();
+				return;
+			}
+
+			resolve();
+		});
+	});
+});
---
+++ test/programs/global-bin/cli/entry.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+// Import and initialize Waterfall CLI
+const waterfall = require('waterfall-cli');
+
+waterfall.init({
+	verbose: true,
+});

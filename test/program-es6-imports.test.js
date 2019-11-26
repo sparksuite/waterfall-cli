fbadebb7ffd809954d5f1a7ec7e35fb027e34bde
Peter <peter@hoa-express.com>
2019-11-26 09:47:18 -0600

    Don't perform this test without having at least v13.2.0 of Node.js

--- test/program-es6-imports.test.js
+++ test/program-es6-imports.test.js
@@ -8,8 +8,8 @@ const semver = require('semver');
 const entryFile = `${__dirname}/programs/es6-imports/cli/entry.js`;
 
 // Don't run the normal tests if older than Node.js version 12
-if (semver.lt(semver.clean(process.version), '12.0.0')) {
-	test('Tests disabled on Node.js versions older than v12', () => {
+if (semver.lt(semver.clean(process.version), '13.2.0')) {
+	test('Tests disabled on Node.js versions older than v13.2.0', () => {
 		expect(true).toBe(true);
 	});
 } else {

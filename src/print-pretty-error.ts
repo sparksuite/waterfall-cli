929ef9ca79ad971751b2f858a65252a5c10bbffa
Peter <peter@hoa-express.com>
2021-02-11 16:59:41 -0600

    Standardize formatting

--- src/print-pretty-error.ts
+++ src/print-pretty-error.ts
@@ -2,7 +2,7 @@
 import chalk from './chalk';
 
 // Print a pretty error message and return just the message
-export default function printPrettyError(message:string) {
+export default function printPrettyError(message: string) {
 	console.error(`${chalk.inverse.red.bold(' ERROR ')}\n`);
 	console.error(chalk.red(`> ${message.split('\n').join('\n> ')}\n`));
 

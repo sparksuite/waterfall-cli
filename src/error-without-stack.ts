a4280be7430b0f6ec0f21258b63087adb066f154
Wes Cossick <wes@sparksuite.com>
2021-03-24 11:43:16 -0500

    Temporarily remove/hide some files

--- src/error-without-stack.ts
+++
@@ -1,7 +0,0 @@
-// Custom error class that does not print a stack
-export default class ErrorWithoutStack extends Error {
-	constructor(...args: string[]) {
-		super(...args);
-		this.stack = this.message;
-	}
-}

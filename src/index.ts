3f56d326ab5b88fb815a232b01ffbda440fadf80
Wes Cossick <wes@sparksuite.com>
2021-03-26 09:53:09 -0500

    Fix exporting

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
-// Imports
+// Export directly
+export { default as init } from './actions/init.js';
+export { default as printError } from './utils/print-pretty-error.js';
+export { PrintableError } from './utils/errors.js';
+
+// Export as default
 import init from './actions/init.js';
 import printError from './utils/print-pretty-error.js';
 import { PrintableError } from './utils/errors.js';
 
-// Default export
 export default {
 	init,
 	// parse,

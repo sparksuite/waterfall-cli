5ccc8b8b8b2207553cab8abd60aa5ae1d8c54ef9
Wes Cossick <wes@sparksuite.com>
2021-03-26 09:52:52 -0500

    Add some comments

--- src/utils/print-pretty-error.ts
+++ src/utils/print-pretty-error.ts
@@ -3,8 +3,10 @@ import chalk from './chalk.js';
 
 // Print a pretty error message and return just the message
 export default function printPrettyError(message: string): string {
+	// Print the error
 	console.error(`${chalk.inverse.red.bold(' ERROR ')}\n`);
 	console.error(chalk.red(message));
 
+	// Return the raw message
 	return message;
 }

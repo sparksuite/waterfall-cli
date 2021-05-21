51be23571fffb8b59e78b50a8b936be8dbea3d14
Peter <peter@hoa-express.com>
2021-05-21 08:37:41 -0500

    Another place to use `isPromiseCallback()` type guard function

--- src/utils/get-organized-arguments.ts
+++ src/utils/get-organized-arguments.ts
@@ -9,6 +9,7 @@ import getMergedSpec from './get-merged-spec.js';
 import chalk from './chalk.js';
 import getAllProgramCommands from './get-all-program-commands.js';
 import Fuse from 'fuse.js';
+import { isPromiseCallback } from './get-command-spec.js';
 
 // Define what organized arguments look like
 interface OrganizedArguments {
@@ -163,9 +164,9 @@ export default async function getOrganizedArguments(): Promise<OrganizedArgument
 						nextValueAccepts =
 							(typeof details.accepts !== 'function'
 								? details.accepts
-								: details.accepts.constructor.name === 'AsyncFunction'
+								: isPromiseCallback(details.accepts)
 								? await details.accepts()
-								: (details.accepts() as string[] | number[])) || undefined;
+								: details.accepts()) || undefined;
 						nextValueType = details.type || undefined;
 						organizedArguments.options.push(option);
 					}
@@ -291,9 +292,9 @@ export default async function getOrganizedArguments(): Promise<OrganizedArgument
 			const accepts =
 				mergedSpec.data.accepts instanceof Array
 					? mergedSpec.data.accepts
-					: mergedSpec.data.accepts instanceof Promise
+					: isPromiseCallback(mergedSpec.data.accepts)
 					? await mergedSpec.data.accepts()
-					: (mergedSpec.data.accepts() as string[] | number[]);
+					: mergedSpec.data.accepts();
 
 			// @ts-expect-error: TypeScript is confused here...
 			if (!accepts.includes(organizedArguments.data)) {

0d95f403acd6261cf9b9572b5f07379258de1a71
Peter <peter@hoa-express.com>
2021-06-17 13:34:24 -0500

    support multiple data items

--- src/utils/get-organized-arguments.ts
+++ src/utils/get-organized-arguments.ts
@@ -287,12 +287,26 @@ export default async function getOrganizedArguments(): Promise<OrganizedArgument
 
 		// Validate data, if necessary
 		if (mergedSpec.data.accepts && mergedSpec.data.accepts instanceof Array) {
-			// @ts-expect-error: TypeScript is confused here...
-			if (!mergedSpec.data.accepts.includes(organizedArguments.data)) {
+			// Find unrecognized data by removing acceptable values
+			let remnants = ` ${organizedArguments.data} `;
+
+			for (const current of mergedSpec.data.accepts) {
+				const find = ` ${current} `;
+				const foundAt = remnants.indexOf(find);
+
+				if (foundAt >= 0) {
+					remnants = remnants.substr(0, foundAt) + remnants.substr(foundAt + find.length - 1);
+				}
+			}
+
+			remnants = remnants.trim();
+
+			// Error if unknown remnants remain
+			if (remnants.length > 0) {
 				throw new PrintableError(
-					`Unrecognized data for "${organizedArguments.command.trim()}": ${
-						organizedArguments.data
-					}\nAccepts: ${mergedSpec.data.accepts.join(', ')}`
+					`Unrecognized data for "${organizedArguments.command.trim()}": ${remnants}\nAccepts: ${mergedSpec.data.accepts.join(
+						', '
+					)}`
 				);
 			}
 		}

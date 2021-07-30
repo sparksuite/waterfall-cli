bd3bf12f2c74865dd234fd131de3d1b3f4f7a4bb
Peter <peter@hoa-express.com>
2021-07-30 10:11:31 -0500

    Enforce accepts being defined when multiple accepts is indicated

--- test-file-trees/bad-structure/cli/invalid-option-js/invalid-option.spec.js
+++ test-file-trees/bad-structure/cli/invalid-option-js/invalid-option.spec.js
@@ -2,8 +2,6 @@ module.exports = {
 	description: 'Order a pizza, to-go',
 	data: {
 		description: 'What type of pizza to order',
-		type: 'fake',
-		acceptsMultiple: true,
 	},
 	options: {
 		test: {

f6fa7adc238546c5f7e7b54362eff6ffd20f788f
Peter <peter@hoa-express.com>
2021-10-14 17:04:14 -0500

    Demonstrate use of `Input` type

--- test-projects/typescript-esm/src/entry.spec.ts
+++ test-projects/typescript-esm/src/entry.spec.ts
@@ -1,6 +1,20 @@
 // Import type
 import { CommandSpec } from 'waterfall-cli';
 
+export interface Input {
+	flags: {
+		['verbose']: boolean;
+	};
+}
+
 // Define and export spec
-const spec: CommandSpec = {};
+const spec: CommandSpec<Input> = {
+	flags: {
+		verbose: {
+			cascades: true,
+			description: 'Output additional details if enabled',
+			shorthand: 'v',
+		},
+	},
+};
 export default spec;
